523d3ec7f71384597d376c447efa4a8a3ec44339
Marcele Montalvão <mmontalvaoblog@outlook.com>
2023-05-02 10:54:04 -0300

    style: GET /booking

--- src/services/booking-service/index.ts
+++ src/services/booking-service/index.ts
@@ -1,26 +1,20 @@
 import httpStatus from 'http-status';
 import enrollmentRepository from '@/repositories/enrollment-repository';
-import { invalidDataError, notFoundError } from '@/errors';
+import { notFoundError } from '@/errors';
 import bookingRepository from '@/repositories/booking-repository';
 import ticketsRepository from '@/repositories/tickets-repository';
 
 async function getBooking(userId: number) {
   const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
-  if (!enrollment) {
-    throw notFoundError();
-  }
   const booking = await bookingRepository.findBookingByUserId(userId);
-
-  if (!booking) {
+  if (!enrollment || !booking) {
     throw notFoundError();
   }
-
   return booking;
 }
 
 async function postBookingRoom(userId: number, roomId: number) {
   const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
-
   if (!enrollment) {
     throw notFoundError();
   }
@@ -28,17 +22,14 @@ async function postBookingRoom(userId: number, roomId: number) {
   if (!ticket || !ticket.TicketType.includesHotel || ticket.TicketType.isRemote || ticket.status === 'RESERVED') {
     throw httpStatus.FORBIDDEN;
   }
-
   const room = await bookingRepository.findRoomById(roomId);
   if (!room) {
     throw notFoundError();
   }
   const bookings = await bookingRepository.findBookingsByRoomId(roomId);
-
   if (room.capacity <= bookings.length) {
     throw httpStatus.FORBIDDEN;
   }
-
   return bookingRepository.createBooking(userId, roomId);
 }
 
